8e339c438ac65adfea68fb5218c378bd4f1a4f50
BearDestiny <119372805+BearDestiny@users.noreply.github.com>
2023-05-24 12:15:34 +0800

    Add files via upload

---
+++ scsp-view/src/main/resources/static/js/head.js
@@ -0,0 +1,19 @@
+//下拉菜单
+// let ecology = document.getElementsByClassName('ecology')[0]
+// let dropdown_ecology = document.getElementsByClassName('dropdown-ecology')[0]
+//
+// ecology.onmouseover = function (){
+//     dropdown_ecology.style.display = 'block'
+// }
+//
+// dropdown_ecology.onmouseover = function (){
+//     dropdown_ecology.style.display = 'block'
+// }
+//
+// ecology.onmouseout = function (){
+//     dropdown_ecology.style.display = 'none'
+// }
+//
+// dropdown_ecology.onmouseout = function (){
+//     dropdown_ecology.style.display = 'none'
+// }
